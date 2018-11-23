5dbffc2340e3b9266216985c95df0108d5fd75b4
Jorge Bernal <jbernal@gmail.com>
2018-11-23 15:20:18 +0100

    Fix bad merge

--- packages/components/src/index.native.js
+++ packages/components/src/index.native.js
@@ -5,7 +5,6 @@ export { default as Toolbar } from './toolbar';
 export { default as ToolbarButton } from './toolbar-button';
 export { default as withSpokenMessages } from './higher-order/with-spoken-messages';
 export { default as IconButton } from './icon-button';
-export { default as ToolbarButton } from './toolbar-button';
 export { createSlotFill, Slot, Fill, Provider as SlotFillProvider } from './slot-fill';
 
 // Higher-Order Components
