c519808a57128437a6386cec0740bad36a7e8bc3
iseulde <wp@iseulde.com>
2018-12-04 15:55:54 +0100

    Don't use isSelected

--- packages/editor/src/components/rich-text/index.js
+++ packages/editor/src/components/rich-text/index.js
@@ -647,7 +647,7 @@ export class RichText extends Component {
 	}
 
 	componentDidUpdate( prevProps, prevState ) {
-		const { tagName, value, isSelected } = this.props;
+		const { tagName, value } = this.props;
 
 		if (
 			tagName === prevProps.tagName &&
@@ -666,7 +666,7 @@ export class RichText extends Component {
 
 			const record = this.formatToValue( value );
 
-			if ( isSelected ) {
+			if ( document.activeElement === this.editableRef ) {
 				record.start = prevState.start;
 				record.end = prevState.end;
 			}
