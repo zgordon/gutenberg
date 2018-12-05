1bf8a639762397559bbd3ea07ad267b426a99428
John Godley <johng75@gmail.com>
2018-12-05 13:23:52 +0000

    Remove mention of freeform block from uni test

--- packages/autop/src/test/index.test.js
+++ packages/autop/src/test/index.test.js
@@ -496,9 +496,9 @@ test( 'that autop doses not add extra closing p in figure', () => {
 	expect( autop( content2 ).trim() ).toBe( expected2 );
 } );
 
-test( 'that autop correctly adds start and end tag when followed by a div', () => {
-	const content = 'Testing freeform block with some\n<div class="wp-some-class">content</div>';
-	const expected = '<p>Testing freeform block with some</p>\n<div class="wp-some-class">content</div>';
+test( 'that autop correctly adds a start and end tag when followed by a div', () => {
+	const content = 'Testing autop with a div\n<div class="wp-some-class">content</div>';
+	const expected = '<p>Testing autop with a div</p>\n<div class="wp-some-class">content</div>';
 
 	expect( autop( content ).trim() ).toBe( expected );
 } );
